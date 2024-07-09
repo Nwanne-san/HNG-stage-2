08b22fec8a1926088ab1f19f66ae67876ceb3bd6
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-07-09 13:34:07 +0100

    correction

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -8,14 +8,16 @@ const playfair = Playfair_Display({subsets: ['latin']})
 export default function heroSection(){
     return (
         <>
-        <div className="flex flex-col w-full text-[#FFFFFF] ">
-            <div className="shop px-6 pb-[44px] pt-14 sm:p-0 sm:h-[471px] flex flex-col gap-4 sm:gap-6 justify-center items-center">
-                <p className={`${playfair.className} text-center text-[20px] sm:text-[32px] font-bold leading-[26px] sm:leading-10`}>Handcrafted Jewelry That Tells Your Unique Story</p>
-                <p className={` sm:text-[18px] text-sm leading-[16px] sm:leading-[21px] sm:p-0 px-[38px] flex text-center  font-normal ${lato.className} `}>
-                    Explore our exclusive collection of artisanal jewelry, meticulously crafted to add a touch of sophistication to every moment. 
-                    From elegant necklaces to stunning custom pieces, find the perfect adornment to celebrate your individuality.
-                </p>
-                <button className={`${lato.className} font-bold px-6 py-4 bg-pink rounded-xl`}>Shop now</button>
+        <div className="flex flex-col w-full  text-[#FFFFFF] ">
+            <div className="shop px-6 pb-[44px] pt-14  sm:h-[471px] flex flex-col  justify-center items-center">
+                <div className="flex flex-col items-center gap-4 sm:gap-6 ">
+                    <p className={`${playfair.className} text-center text-[20px] sm:text-[32px] font-bold leading-[26px] sm:leading-10`}>Handcrafted Jewelry That Tells Your Unique Story</p>
+                    <p className={` sm:text-[18px] text-sm leading-[16px] sm:leading-[21px] lg:w-[694px] px-[38px] flex text-center  font-normal ${lato.className} `}>
+                        Explore our exclusive collection of artisanal jewelry, meticulously crafted to add a touch of sophistication to every moment.
+                        From elegant necklaces to stunning custom pieces, find the perfect adornment to celebrate your individuality.
+                    </p>
+                    <button className={`${lato.className} font-bold px-6 py-4 text-center bg-pink rounded-xl`}>Shop now</button>
+                </div>
             </div>
             <div className="flex sm:flex-row flex-col w-full">
                 <div className="flex w-full">
